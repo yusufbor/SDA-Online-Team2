23704f680f7a5fbe95f0833134819387a45908a3
yusufbor <ysfbr23@gmail.com>
2024-03-10 13:00:44 +0300

    second commit

--- src/test/java/Homework/TryingNew.java
+++ src/test/java/Homework/TryingNew.java
@@ -3,5 +3,6 @@ package Homework;
 public class TryingNew {
     public static void main(String[] args) {
         System.out.println("This is a new class");
+        System.out.println("second message");
     }
 }
